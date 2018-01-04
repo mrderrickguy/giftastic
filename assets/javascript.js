ed4e5b6b404c8b4572f08aa5e912b22f4a24f6b3
mrderrickguy <zakiahdg@gmail.com>
2018-01-03 18:13:41 -0600

    I was missing a space in my code

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,5 +1,5 @@
 $(document).ready( function(){
-	
+
 	generateButtons(boxArray,'searchButton','#buttonArea');
 })
 var boxArray= ['Food','Cars','Art'];
@@ -7,7 +7,7 @@ var boxArray= ['Food','Cars','Art'];
 function generateButtons(boxArray,addSomeClass,placeToAdd){
 
 $(placeToAdd).empty();
-for (var i=0; i<boxArray.length; i++){
+for (var i=0; i<boxArray.length;i++){
 	
 	var a = $('<button>');
 	a.addClass(addSomeClass);
@@ -18,28 +18,34 @@ for (var i=0; i<boxArray.length; i++){
 
 }
 $(document).on('click','.searchButton',function(){
+	var type= $(this).data('type');
 	$('#searches').empty();
-	var type= $(this).data();
+	
 
 	var queryURL = 'https://api.giphy.com/v1/gifs/search?q='+type+'&api_key=8aXAx6k4WjWlOAP38X525fn2wTYO5xb1&limit=5';
-	$.ajax({url:queryURL,method:'GET'})
+	$.ajax({
+		url:queryURL,
+		method:'GET'})
 		.done(function(response){
-			for(var i=0; i< response.data.length;i++){
-				var search = $('<div>');
-				var ratings = response.data[i].rating;
-				var h1 = $('<p>').text('rating:'+ratings);
+
+			for(var i=0;i<response.data.length;i++){
+				var search = $('<div class="search-input">');
+				var rating = response.data[i].rating;
+				var p = $('<p>').text('Rating: '+rating);
 				var animated = response.data[i].images.fixed_height.url;
 				var still = response.data[i].images.fixed_height_still.url;
-				var image = $('<img>');
+				var image = $('<img src='+ still+'>');
 				image.attr('src',still);
 				image.attr('data-still',still);
 				image.attr('data-animated',animated);
 				image.attr('data-state','still');
+				image.attr('data-state','animated');
 				image.addClass('searchImage');
-				search.append(h1);
+				search.append(p);
 				search.append(image);
-				$('#searches').append(search);
-				console.log(search);
+				$('#searches').append(image);
+				
+				console.log(still);
 			}
 
 		})
